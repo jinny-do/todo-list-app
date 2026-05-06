81f047afd65cdd7bcea20a29902b295513499ad0
jinny-do <sonmojiri7@gmail.com>
2026-05-06 17:43:13 +0900

    프로필 이름 이메일 연결

--- todolist-server/routes/user.js
+++ todolist-server/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const { verifyToken } = require("../middleware/auth");
 
-router.get("/:id", userController.getUser);
 router.get("/me", verifyToken, userController.getMyProfile);
+router.get("/:id", userController.getUser);
 
 module.exports = router;
