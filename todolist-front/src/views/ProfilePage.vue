8fa7721e94f21221fd2049d20405b2ec87bed0ed
jinny-do <sonmojiri7@gmail.com>
2026-05-08 17:56:33 +0900

    260508 프로필 수정

--- todolist-front/src/views/ProfilePage.vue
+++ todolist-front/src/views/ProfilePage.vue
@@ -20,15 +20,22 @@ onMounted(async () => {
 <template>
   <div class="container">
     <div class="profile">
-      <h2 class="profile-header-text">📝{{ name }}님의 TODO LIST</h2>
+      <router-link to="/todo" class="profile-header-link">
+        <h2 class="profile-header-text">📝{{ name }}님의 TODO LIST</h2>
+      </router-link>
       <div class="profile-box">
         <div class="profile-img-box">
           <div class="profile-img"></div>
         </div>
-        <div class="user-info">
-          <p>이름 : {{ name }}</p>
-          <p>이메일 : {{ email }}</p>
+        <div class="user-info-box">
+          <div class="user-info">
+            <p>이름 : {{ name }}</p>
+            <p>이메일 : {{ email }}</p>
+          </div>
+
+          <button type="button" class="re-btn">회원정보 수정</button>
         </div>
+
         <img src="/img/pen.png" alt="연필" class="pen" />
       </div>
     </div>
@@ -41,6 +48,8 @@ onMounted(async () => {
 }
 
 .profile {
+  margin: auto;
+  width: 800px;
   padding: 20px;
   background-color: #c89b5a;
   border-radius: 20px;
@@ -48,6 +57,15 @@ onMounted(async () => {
   box-shadow:
     rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
     rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+}
+
+.profile-header-link {
+  text-decoration: none;
+}
+
+.profile-header-text:hover {
+  color: var(--bg-color);
+  transition: all 0.3s;
 }
 
 .profile-header-text {
@@ -80,17 +98,41 @@ onMounted(async () => {
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
 }
 
-.user-info {
+.user-info-box {
   font-size: 30px;
   padding: 50px;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   gap: 10px;
   background-color: #fff;
   border-radius: 10px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
   border: 5px solid #8b5e3c;
   text-align: center;
+}
+
+.user-info {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.re-btn {
+  padding: 10px;
+  font-size: 20px;
+  font-weight: 600;
+  border-radius: 10px;
+  border: none;
+  background-color: #b38b54;
+  color: #fff;
+  cursor: pointer;
+}
+
+.re-btn:hover {
+  background-color: #8b5e3c;
+  transition: all 0.3s;
 }
 
 .pen {
